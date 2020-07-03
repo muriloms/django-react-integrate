41a842e4d1e1c86658c78411b457e48134eff387
muriloms <ms7murilo@gmail.com>
2020-07-03 17:45:13 -0300

    create project

---
+++ frontend/gui/src/containers/ArticleListView.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+import Articles from '../components/Article';
+
+class ArticleList extends React.Component {
+    render() {
+        return (
+            <Articles />
+        )
+    }
+}
+
+export default ArticleList;
